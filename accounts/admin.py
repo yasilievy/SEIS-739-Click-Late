2cdebadc16f53a052cd7fcff69047254ea314e70
yasilievy <yvyang13@outlook.com>
2025-03-21 00:54:06 -0500

    final touches for the stopping point

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import UserProfile
+# from .models import UserProfile
 
-# if a new database table is added, you have to register here
-admin.site.register(UserProfile)
+# # if a new database table is added, you have to register here
+# admin.site.register(UserProfile)
--- accounts/models.py
+++ accounts/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
-class UserProfile(models.Model):
-    id = models.IntegerField(primary_key=True)
-    firstname = models.CharField(max_length=40)
-    lastname = models.CharField(max_length=40)
-    email = models.CharField(max_length=40)
-    username = models.CharField(max_length=40)
-    password = models.CharField(max_length=40)
+# class UserProfile(models.Model):
+#     id = models.IntegerField(primary_key=True)
+#     firstname = models.CharField(max_length=40)
+#     lastname = models.CharField(max_length=40)
+#     email = models.CharField(max_length=40)
+#     username = models.CharField(max_length=40)
+#     password = models.CharField(max_length=40)
