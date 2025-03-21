14f337fa2df06f5fcd8432f5b5d5f407f444fa63
yasilievy <yvyang13@outlook.com>
2025-03-21 01:13:29 -0500

    added reset page, and link in login

--- accounts/urls.py
+++ accounts/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('register/', views.register_view, name='register'),
     path('logout/', views.logout_view, name='logout'),
+    path('password_reset/', views.password_reset_request, name='password_reset')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, PasswordResetForm
 from django.contrib import messages
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import send_mail
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.template.loader import render_to_string
 from .forms import CreateUserForm
 
 
@@ -43,3 +48,37 @@ def logout_view(request):
     logout(request)
     messages.info(request, 'You have successfully logged out.')
     return redirect('login')
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+# Password Reset Request view
+def password_reset_request(request):
+    if request.method == 'POST':
+        form = PasswordResetForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            # associated_user = User.objects.filter(email=email)
+            # if associated_user.exists():
+            #     user = associated_user[0]
+            #     token = default_token_generator.make_token(user)
+            #     uid = urlsafe_base64_encode(str(user.pk).encode())
+            #     domain = get_current_site(request).domain
+            #     link = f'http://{domain}/password_reset_confirm/{uid}/{token}/'
+
+            #     # Send reset password email
+            #     send_mail(
+            #         'Password Reset Request',
+            #         f'Click the following link to reset your password: {link}',
+            #         'no-reply@mywebsite.com',
+            #         [email],
+            #         fail_silently=False,
+            #     )
+            #     messages.success(request, 'Password reset email sent!')
+            #     return redirect('login')
+            # else:
+            #     messages.error(request, 'Email not found')
+    else:
+        form = PasswordResetForm()
+    return render(request, 'accounts/password_reset.html', {'form': form})
