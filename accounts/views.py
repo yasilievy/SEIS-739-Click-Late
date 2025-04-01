9fea9f305fa9e4c3a8b1689ea397abcccd31460a
yasilievy <yvyang13@outlook.com>
2025-04-01 13:09:31 -0500

    some form of nav bar implemented

--- accounts/views.py
+++ accounts/views.py
@@ -34,7 +34,7 @@ def login_view(request):
             if user is not None:
                 login(request, user)
                 messages.success(request, f'Welcome back, {username}!')
-                return redirect('home')  # Redirect to the home page
+                return redirect('home-user',username=user.username)  # Redirect to the home page
             else:
                 messages.error(request, 'Invalid username or password')
         else:
--- clicklate/views.py
+++ clicklate/views.py
@@ -5,7 +5,12 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from django.shortcuts import render
+from django.contrib.auth.models import User
+
 
 def home(request):
     return render(request, 'home.html')
 
+def home_user(request,username):
+    user = User.objects.filter(username=username)[0]
+    return render(request, 'home-user.html',{'user':user})
